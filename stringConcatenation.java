488108447b726eda1d97f4a32c53272fac50b42c
bader <bader5uk@hotmail.com>
2024-09-25 12:58:28 +0400

    code compares 2 strings and equates the same length

---
+++ stringConcatenation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class stringConcatenation {
+
+    public static void main(String[] args) {
+        scanner();
+    }
+    public static void scanner() {
+        Scanner firstScanner = new Scanner(System.in);
+        System.out.println("Enter the first String: ");
+        String firstString = firstScanner.nextLine();
+        Scanner secondScanner = new Scanner(System.in);
+        System.out.println("Enter the first String: ");
+        String secondString = secondScanner.nextLine();
+        int size1 = firstString.length();
+        int size2 = secondString.length();
+        int minimumlength = Math.min(size1, size2);
+        String result1 = firstString.substring(size1 - minimumlength);
+        String result2 = secondString.substring(size2 - minimumlength);
+        System.out.println(result1 + result2);
+    }
+}
+
+
